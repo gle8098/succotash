8b1e6df17bf55eace24d3f2fded786f464b76f01
houcha <baiuk.is@phystech.edu>
2020-04-27 18:00:20 +0300

    Update main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -21,20 +21,22 @@ int main() {
                    int(button->GetShape().getSize().x) << "," <<
                    int(button->GetShape().getSize().y) << std::endl;
     };
-  auto button1 = new Button("Button #1", button_action);
-  auto button2 = new Button("Button #2", button_action);
-  auto button3 = new Button("Button #3", button_action);
 
-  auto buttons = new View;
-  buttons->AddSon(button1);
-  buttons->AddSon(button2);
-  buttons->AddSon(button3);
+  const size_t buttons_cnt = 3;
+  Button* buttons[buttons_cnt];
+  auto button_context = new View;
+
+  for (size_t i = 0; i < buttons_cnt; ++i) {
+    sf::String name = "Button #" + std::to_string(i);
+    buttons[i] = new Button(name, button_action);
+    button_context->AddSon(buttons[i]);
+  }
 
   LinearLayout layout1(LinearLayout::Type::Vertical);
   layout1.SetWeights({4, 2, 3});
-  buttons->SetLayout(&layout1);
+  button_context->SetLayout(&layout1);
 
-  master_view.AddSon(buttons);
+  master_view.AddSon(button_context);
 
   LinearLayout layout2(LinearLayout::Type::Vertical);
   master_view.SetLayout(&layout2);
