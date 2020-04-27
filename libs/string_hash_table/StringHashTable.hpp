11e0e78ac71c40869f5b482ad7b1455be2557b8d
supercomputer <gle8098@yandex.ru>
2020-04-27 04:03:09 +0300

    Project clean-up

---
+++ libs/string_hash_table/StringHashTable.hpp
@@ -0,0 +1,9 @@
+#ifndef SUCCOTASH_STRINGHASHTABLE_HPP
+#define SUCCOTASH_STRINGHASHTABLE_HPP
+
+#include <map>
+
+template <typename T>
+using StringHashTable = std::map<std::string, T>;
+
+#endif // SUCCOTASH_STRINGHASHTABLE_HPP
