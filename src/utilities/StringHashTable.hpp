5f57a332ca0051059bdaf449f49907dc79438a40
supercomputer <gle8098@yandex.ru>
2020-05-03 19:37:15 +0300

    Extended StringHashTable. Replaced 'using' with inheritance. Yes, there are arguments for it

--- src/utilities/StringHashTable.hpp
+++ src/utilities/StringHashTable.hpp
@@ -6,7 +6,25 @@
 namespace succotash::utilities {
 
 template <typename T>
-using StringHashTable = std::unordered_map<std::string, T>;
+class StringHashTable
+    : public std::unordered_map<std::string, T, std::hash<std::string>,
+                                std::equal_to<std::string>,
+                                std::allocator<std::pair<const std::string, T>>>
+{
+ public:
+
+  // Sorry, bro. This cannot be compiled :(
+  // std::optional of a reference is not supported
+  /*
+  std::optional<T&> get(const std::string& key) const {
+    auto it = this->find(key);
+    if (it == this->end()) {
+      return {};
+    }
+    return std::optional<T&>(*it);
+  }
+  */
+};
 
 }
 
